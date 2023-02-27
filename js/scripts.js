5c846bee3b1d2964e645599c0879d2d437389912
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-02-27 13:50:55 -0700

    Update scripts.js
    second attempt at a conditional

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,10 @@ let pokemonList = [
     {name: "Ivysaur", height: 1, types: ["grass","poison"]}, 
     {name: "Venusaur", height: 2, types: ["grass","poison"]}
 ]; 
-let result 
-for (let i=0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name +" "+"(Height: "+pokemonList[i].height+" )");
-    if (pokemonList.height > 1); { 
-        document.write("Wow, that's big! "); 
-    }   
-};
 
+for (let i=0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name +" "+"(Height: "+pokemonList[i].height+") ");
+    if (pokemonList.height > 1); { 
+        document.write("Wow, that's big! ");   
+    };
+}
