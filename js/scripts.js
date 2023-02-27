ada192d272197f4c98b21c203e9767675a277b8c
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-02-26 18:42:15 -0700

    js script update
    An attempt at my first for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-let pokemonList = [{name: "Bulbasaur", height: 7, types: ["grass","poison"]}, {name: "Ivysaur", height: 1, types: ["grass","poison"]}, {name: "Venusaur", height: 2, types: ["grass","poison"]}]; 
+let pokemonList = [
+    {name: "Bulbasaur", height: 7, types: ["grass","poison"]}, 
+    {name: "Ivysaur", height: 1, types: ["grass","poison"]}, 
+    {name: "Venusaur", height: 2, types: ["grass","poison"]}
+]; 
+for (let i=0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + height);
+};
