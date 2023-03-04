e2cbbf495ad491e208f88982d4e519d6fc8340a1
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-03-04 15:44:14 -0700

    scripts update
    replaced for loop with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-let pokemonList = [
+const pokemonList = [
     {name: "Bulbasaur", height: 7, types: ["grass","poison"]}, 
     {name: "Ivysaur", height: 1, types: ["grass","poison"]}, 
     {name: "Venusaur", height: 2, types: ["grass","poison"]}
 ]; 
 
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height >1){ 
-        document.write(pokemonList[i].name +" "+"(Height: "+pokemonList[i].height+") "+"wow, that's big");  
-  } else if(pokemonList[i].height < 2){
-        document.write(pokemonList[i].name +" "+"(Height: "+pokemonList[i].height+") ");
-  }
-}
+pokemonList.forEach((element, index, array) => {
+    document.write(element.name + ")" + " " + "Height: " + element.height + "," + " Types: " + element.types); 
+    document.write("<br/>")
+});
+
+
+
