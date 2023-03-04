4184e78c64234b988a4494238c180d1d92adc599
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-03-04 15:54:50 -0700

    Update scripts.js
    I added another version of the forEach loop that matched up with the directions of the task better

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,13 @@ const pokemonList = [
     {name: "Venusaur", height: 2, types: ["grass","poison"]}
 ]; 
 
-pokemonList.forEach((element, index, array) => {
+/*pokemonList.forEach((element, index, array) => {
     document.write(element.name + ")" + " " + "Height: " + element.height + "," + " Types: " + element.types); 
     document.write("<br/>")
-});
+});*/
 
-
-
+pokemonList.forEach(function(pokemon) {
+      //document.write(pokemon.name + ' is ' + pokemon.height + ' years old.');
+      document.write(pokemon.name + ")" + " " + "Height: " + pokemon.height + "," + " Types: " + pokemon.types); 
+    document.write("<br/>")
+    });
