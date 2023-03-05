363cad84cc65db8c9c04a937bbd36f9bae265fd7
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-03-04 21:31:18 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,41 @@
-const pokemonList = [
-    {name: "Bulbasaur", height: 7, types: ["grass","poison"]}, 
-    {name: "Ivysaur", height: 1, types: ["grass","poison"]}, 
-    {name: "Venusaur", height: 2, types: ["grass","poison"]}
-]; 
+let pokemonRepository = (function () {
 
-/*pokemonList.forEach((element, index, array) => {
-    document.write(element.name + ")" + " " + "Height: " + element.height + "," + " Types: " + element.types); 
-    document.write("<br/>")
-});*/
+      let pokemonList = [
+      {name: "Bulbasaur", height: "7 m", types: ["grass","poison"]}, 
+      {name: "Ivysaur", height: "1 m", types: ["grass","poison"]}, 
+      {name: "Venusaur", height: "2 m", types: ["grass","poison"]}
+      ]; 
 
-pokemonList.forEach(function(pokemon) {
-      //document.write(pokemon.name + ' is ' + pokemon.height + ' years old.');
-      document.write(pokemon.name + ")" + " " + "Height: " + pokemon.height + "," + " Types: " + pokemon.types); 
-    document.write("<br/>")
-    });
+      /*pokemonList.forEach((element, index, array) => {
+      document.write(element.name + ")" + " " + "Height: " + element.height + "," + " Types: " + element.types); 
+      document.write("<br/>")
+      });
+
+      pokemonList.forEach(function(pokemon) {
+            document.write(pokemon.name + ")" + " " + "Height: " + pokemon.height + "," + " Types: " + pokemon.types); 
+            document.write("<br/>")
+      });*/
+
+      function getAll () {
+            return pokemonList;
+      }
+      function add (pokemon) {
+            pokemonList.push(pokemon);
+      }
+
+      return{
+            getAll: getAll,
+            add: add
+      }
+
+})();
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: 'Pikachu', height: ".4 m", types: ["electric"] });
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+
+pokemonRepository.getAll(pokemon); 
+pokemonList.forEach((pokemon) => {
+      document.write(pokemon.name + ")" + " " + "Height: " + pokemon.height + "," + " Types: " + pokemon.types);
+      document.write("<br/>");
+});
