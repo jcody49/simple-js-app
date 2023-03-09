80bb7fb9e0b998244be1d643b47a578dd5b5a95b
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-03-09 16:16:19 -0700

    Updates to css, js, and html
    This is an attempt to create buttons and apply stylings with document.select

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,21 @@ console.log(pokemonRepository.getAll()); // []
 pokemonRepository.add({ name: 'Pikachu', height: ".4 m", types: ["electric"] });
 console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
 
-pokemonRepository.getAll().forEach((pokemon) => {
+/*pokemonRepository.getAll().forEach((pokemon) => {
       document.write(pokemon.name + ")" + " " + "Height: " + pokemon.height + "," + " Types: " + pokemon.types); 
             document.write("<br/>")
+})*/
+
+
+pokemonRepository.getAll().forEach((pokemon) => {
+      let pokemon = document.querySelector('.pokemon-list'); 
+      let listItem = document.createElement('li')
+      let button = document.createElement(button.classList.add(pokemonButtons))
 })
+
+
+
+
+
+
+//let element = document.querySelector('.pokemon-list'); 
