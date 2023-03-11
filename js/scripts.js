61d3a30e95b222d493e9e26219b2ded0e0d5fb67
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-03-11 15:06:10 -0700

    update to index.html, js.sripts, and css
    These changes will:
    
    manipulate the DOM-- take the pokemon objects and turn them into buttons.
    The buttons now have event listeners and are console logging the pokemon when clicked.
    
    css now has styling for the pokemon buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -9,38 +9,47 @@ let pokemonRepository = (function () {
       function getAll () {
             return pokemonList;
       }
+
+
       function add (pokemon) {
-            pokemonList.push(pokemon);
+            pokemonList.push(pokemon);  
       }
+
+      /*function add (pokemon) {
+            if (typeof pokemon === Object && Object.keys === name, height, types) {
+                  pokemonList.push(pokemon);
+            } 
+      }*/
+
+      function addListItem(pokemon) {
+            let pokemonList = document.querySelector('.pokemon-list'); 
+            let listItem = document.createElement('li');
+            let button = document.createElement('button');
+            button.innerText = pokemon.name;
+            button.classList.add('pokemonButtons')
+            listItem.appendChild(button);
+            pokemonList.appendChild(listItem);
+            button.addEventListener('click', function () {
+                  showDetails(pokemon)
+            });
+      }
+
+      function showDetails(pokemon) {
+            console.log(pokemon)
+      }
+
 
       return{
             getAll: getAll,
-            add: add
+            add: add,
+            addListItem: addListItem
       }
-
 })();
 console.log(pokemonRepository.getAll()); // []
 pokemonRepository.add({ name: 'Pikachu', height: ".4 m", types: ["electric"] });
 console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
-
-/*pokemonRepository.getAll().forEach((pokemon) => {
-      document.write(pokemon.name + ")" + " " + "Height: " + pokemon.height + "," + " Types: " + pokemon.types); 
-            document.write("<br/>")
-})*/
-
+//pokemonRepository.addListItem(pokemon);
 
 pokemonRepository.getAll().forEach((pokemon) => {
-      let element = document.querySelector('pokemon-list'); 
-      let listItem = document.createElement('li');
-      let button = document.createElement(button.classList.add(pokemonButtons));
-      button.innerText = pokemon.name;
-      li.appendChild(button)
-      unorderedList.appendChild(listItem)
-})
-
-
-
-
-
-
-//let element = document.querySelector('.pokemon-list'); 
+      pokemonRepository.addListItem(pokemon);
+});
